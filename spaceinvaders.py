e0c67acece19f120cd0e7f072fb0f619407a521f
loganGr23 <33901648+loganGr23@users.noreply.github.com>
2017-12-05 06:19:36 -0800

    Update spaceinvaders.py

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -22,7 +22,20 @@ class Sprite(turtle.Turtle):
     def move(self):
         self.fd(self.speed)
 
+        #Boundary detection
+        if self.xcor() > 290:
+            self.setx(-90)
+            self.rt(60)
+        if self.xcor() < -290:
+            self.setx(-290)
+            self.rt(60)
 
+        if self.ycor() > 290:
+            self.sety(290)
+            self.rt(60)
+        if self.ycor() < -290:
+            self.sety(-290)
+            self.rt(60)
 
 class Player(Sprite):
     def __init__(self, spriteshape, color, startx, starty):
@@ -42,16 +55,45 @@ class Player(Sprite):
     def decelerate(self):
         self.speed -= 1
 
+class Game():
+    def __init__(self):
+        self.level = 1
+        self.score = 0
+        self.state = "playing"
+        self.pen = turtle.Turtle()
+        self.lives = 3
+
+
+#Border
+    def draw_border(self):
+        self.pen.speed(0)
+        self.pen.color("white")
+        self.pen.pensize(3)
+        self.pen.penup()
+        self.pen.goto(-300,300)
+        self.pen.pendown()
+        for side in range(4):
+            self.pen.fd(600)
+            self.pen.rt(90)
+        self.pen.penup()
+        self.pen.ht()
+
+#Create game object
+game = Game()
+
+#Draw the game border
+game.draw_border()
+
 #Create my sprites
 player = Player("triangle", "white", 0,0)
 
 #Keyboard bindings
 turtle.listen()
-turtle.onkey(player.turn_left, "Left")
-turtle.onkey(player.turn_right, "Right")
-turtle.onkey(player.accelerate, "Up")
-turtle.onkey(player.decelerate, "Down")
+turtle.onkey(player.turn_left, "a")
+turtle.onkey(player.turn_right, "d")
+turtle.onkey(player.accelerate, "w")
+turtle.onkey(player.decelerate, "s")
 
 #Game Loop
 while True:
-    player.mov
+    player.move()
